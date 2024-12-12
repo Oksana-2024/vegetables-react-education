91fd2afa587b28fdc6949069c99d6d06d18bad62
Oksana <oksanavasilevna77@gmail.com>
2024-12-12 03:24:25 +0200

    update index.css and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import css from "./App.module.css";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Hero from "./Hero/Hero";
+import MobileMenu from "./MobileMenu/MobileMenu";
+import clsx from "clsx";
+import Vegetables from "./Vegetables/Vegetables";
 //import style from "../index.css"
 //import textDiscount from "../textDiscount.json"
 //import Discount from "./Discount/Discount";
@@ -14,13 +18,23 @@ const App = () => {
         </div>
       </section>
       <main>
-        <section className={css.heroPage}></section>
+        <section className={css.heroPage}>
+          <div className={clsx(css.container, css.heroContainer)}>
+            <Hero/>
+          </div>
+        </section>
+        <section className={css.vegetablesPage}>
+          <div className={clsx(css.container, css.vegetablesContainer)}>
+            <Vegetables/>
+          </div>
+        </section>
       </main>
       <section className={css.footerPage}>
-        <div className={css.container}>
+        <div className={clsx(css.container, css.footerContainer)}>
           <Footer />
         </div>
       </section>
+      <MobileMenu />
     </>
   );
 };
