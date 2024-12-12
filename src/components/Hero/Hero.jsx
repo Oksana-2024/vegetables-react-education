021b2ab13cec73e98b2e35b0339e76046e0e0e47
Oksana <oksanavasilevna77@gmail.com>
2024-12-12 03:22:11 +0200

    add hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+  return (
+    <>
+      <h1>organic vegetables to your diet today!</h1>
+      <p>
+        Our expertly curated vegetable baskets are made with the freshest,
+        highest quality vegetables available. Fresh Harvest Box has got you
+        covered. Fresh, high-quality vegetables in expertly curated vegetable
+        baskets delivered to you.
+      </p>
+      <button type="button">shopnow</button>
+      <img src="" alt="" />
+      <button type="button">#organic</button>
+      <button type="button">#products</button>
+      <button type="button">#basket</button>
+      <button type="button">#vegetables</button>
+    </>
+  );
+};
+export default Hero;
