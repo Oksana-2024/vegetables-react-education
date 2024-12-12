a6eb210d4d8e784851b4b01209f5016b4f03f6fa
Oksana <oksanavasilevna77@gmail.com>
2024-12-12 03:21:35 +0200

    add component and styles for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
+import Social from "../Social/Social";
+import css from "./Footer.module.css";
 //import css from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <>
       <Logo />
-      <Navigation header={false}/>
+      <Navigation navFooter={true} footer={true} />
+     <Social/>
+      <address className={css.address} id="contacts">
+        <a href="tel:+380684439426">+380 (68) 443-94-26</a>
+        <br />
+        1678 S. Pioneer Road <br /> Salt Lake City <br /> UT 84104
+      </address>
     </>
   );
 };
