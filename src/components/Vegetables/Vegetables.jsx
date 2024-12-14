808e1598e63c959bd69c75db3e2e076b4f1e6336
Oksana <oksanavasilevna77@gmail.com>
2024-12-14 02:23:01 +0200

    add section vegetables

--- src/components/Vegetables/Vegetables.jsx
+++ src/components/Vegetables/Vegetables.jsx
@@ -1,4 +1,5 @@
 import css from "./Vegetables.module.css";
+import a from "../../assets/images/img_vegetables.png";
 
 const Vegetables = () => {
   return (
@@ -6,7 +7,8 @@ const Vegetables = () => {
       <h2 className={css.title}>
         To order your vegetable basket, simply follow these easy steps
       </h2>
-      <p className={css.text}>
+      <img className={css.image} src={a} alt="Broccoli" width={335} height={158} />
+      <p className={css.text1}>
         Our baskets are assembled with care and delivered straight to your
         doorstep, so you can enjoy the taste of fresh fruit without ever leaving
         your home.
