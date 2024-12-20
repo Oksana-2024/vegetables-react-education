f000c510d5e8b69cdab01a550f9764053b590c96
Oksana <oksanavasilevna77@gmail.com>
2024-12-20 18:07:15 +0200

    add animation for SectionDiscount

--- src/components/SectionDiscount/SectionDiscount.jsx
+++ src/components/SectionDiscount/SectionDiscount.jsx
@@ -1,41 +1,22 @@
+import Marquee from "react-fast-marquee";
 import css from "./SectionDiscount.module.css";
 
 const SectionDiscount = () => {
   return (
     <section className={css.discountPage}>
-     <h2 className={css.hidden}>Discount</h2>
-           <div className={css.firstLine}>
-             <ul className={css.list}>
-               <li className={css.text}>
-                 Discount up to <span>40%</span>! Taste <span>the vitamins</span> and keep your
-                 health.
-               </li>
-               <li className={css.text}>
-                 Discount up to <span>40%</span>! Taste <span>the vitamins</span> and keep your
-                 health.
-               </li>
-               <li className={css.text}>
-                 Discount up to <span>40%</span>! Taste <span>the vitamins</span> and keep your
-                 health.
-               </li>
-             </ul>
-           </div>
-           <div className={css.secondLine}>
-             <ul className={css.list}>
-               <li className={css.text}>
-                 Discount up to <span>40%</span>! Taste <span>the vitamins</span> and keep your
-                 health.
-               </li>
-               <li className={css.text}>
-                 Discount up to <span>40%</span>! Taste <span>the vitamins</span> and keep your
-                 health.
-               </li>
-               <li className={css.text}>
-                 Discount up to <span>40%</span>! Taste <span>the vitamins</span> and keep your
-                 health.
-               </li>
-             </ul>
-           </div>
+      <h2 className={css.hidden}>Discount</h2>
+      <div className={css.firstLine}>
+        <Marquee autoFill>
+          Discount up to <span>40%</span>! Taste <span>the vitamins</span> and
+          keep your health.
+        </Marquee>
+      </div>
+      <div className={css.secondLine}>
+        <Marquee autoFill direction={"right"}>
+          Discount up to <span>40%</span>! Taste <span>the vitamins</span> and
+          keep your health.
+        </Marquee>
+      </div>
     </section>
   );
 };
