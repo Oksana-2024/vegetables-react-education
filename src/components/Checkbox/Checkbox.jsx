ef15b2dfbc4920418826e7e748f024f36b798c19
Oksana <oksanavasilevna77@gmail.com>
2025-01-04 13:52:42 +0200

    add Checkbox component

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+import { useId, useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import s from "./Checkbox.module.css";
+
+const Checkbox = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const inputId = useId()
+  const handleChange = () => {
+    setIsChecked(!isChecked);
+  };
+  return (
+    <>
+      <label htmlFor={inputId}>
+        <span className={s.iconCkecked}>
+          <FaCheck size={8} className={clsx(isChecked ? s.iconC : s.icon)} />
+        </span>
+      </label>
+      <input
+        type="checkbox"
+        name="checkbox"
+        id={inputId}
+        className={s.visuallyHidden}
+        value={isChecked}
+        onChange={handleChange}
+      />
+    </>
+  );
+};
+export default Checkbox;
